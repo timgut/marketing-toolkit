d9e5e5b96a14332d61edd6ba1f9cb61fbd71a818
Jarred Trost <jtrost@gmail.com>
2017-01-23 12:50:38 -0600

    Added paperclip gem

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,6 @@
 class Image < ApplicationRecord
+  has_attached_file :image
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
   has_and_belongs_to_many :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -62,8 +62,12 @@ ActiveRecord::Schema.define(version: 20170118214749) do
   end
 
   create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
   create_table "images_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
