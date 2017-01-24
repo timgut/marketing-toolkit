d88cd1dbfc8a82d68d9edfd3c372a15785f97173
Jarred Trost <jtrost@gmail.com>
2017-01-24 10:47:48 -0600

    Use around_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :set_current_user
+  around_action :set_current_user
 
   protected
 
   def set_current_user
     User.current_user = User.first
+    yield
+    User.current_user = nil
   end
 end
