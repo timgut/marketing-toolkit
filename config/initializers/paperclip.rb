204f77694fa371d68f1a1d627edd21617ec233e6
Tim Gutowski <tgutowski@trilogyinteractive.com>
2017-01-26 13:58:59 -0600

    got a dynamic path working paperclip

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,6 @@
-Paperclip.interpolates :path do |attachment, style|
-  attachment.instance.folder.path
+Paperclip.interpolates :dynamic_path do |attachment, style|
+  path = attachment.instance.folder.path
+  file_name = attachment.instance.image_file_name
+  folder = attachment.instance.folder.path == '/' ? "root" : attachment.instance.folder.path
+  "/images/#{folder}/#{style}/#{file_name}"
 end
