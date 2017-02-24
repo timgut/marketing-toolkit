c6c0c1c14988e714803ebb01a5eab2654281843b
Tim Gutowski <tgutowski@trilogyinteractive.com>
2017-02-24 14:42:54 -0600

    added a devise layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,24 @@ class ApplicationController < ActionController::Base
 
   around_action :set_current_user
 
+  layout :layout_by_resource
+
   protected
 
   def set_current_user
     User.current_user = User.first
     yield
     User.current_user = nil
+  end
+
+  private
+  #https://github.com/plataformatec/devise/wiki/How-To:-Create-custom-layouts
+  def layout_by_resource
+  	if devise_controller?
+  		"devise"
+  	else
+  		"application"
+  	end
   end
 end
  
