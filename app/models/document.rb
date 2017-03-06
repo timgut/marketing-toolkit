95efed664ecbc4b88e73985e76dc0394cdf758ee
Tim Gutowski <tgutowski@trilogyinteractive.com>
2017-03-06 15:45:34 -0600

    documents/recent shows only my documents now

--- app/models/document.rb
+++ app/models/document.rb
@@ -14,7 +14,7 @@ class Document < ApplicationRecord
 
   validates_presence_of :template, :title, :description, :status
 
-  scope :recent,         ->{ where("created_at >= ?", DateTime.now - 1.month) }
+  scope :recent,         ->{ all.joins(:documents_users).where("creator_id = ? and documents_users.created_at >= ?", User.current_user.id, DateTime.now - 1.month) }
   scope :shared_with_me, ->{ all.joins(:documents_users).where("documents_users.creator_id != ?", User.current_user.id) }
 
   attr_accessor :defined_data_methods
