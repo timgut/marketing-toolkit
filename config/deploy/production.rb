26efd6b64007da92072d909ce96276e7777c7b6d
Tim Gutowski <tgutowski@trilogyinteractive.com>
2017-03-10 12:32:33 -0600

    production deploy changes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,4 +6,7 @@ set :rails_env,   "production"
 set :deploy_to,   "/data/#{fetch(:application)}-prod"
 set :deploy_user, "app-#{fetch(:application)}"
 
+
+set :branch, "master"
+
 server "chi-rails1", user: 'app-afscme-tk', roles: %w(web app db)
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,4 +6,6 @@ set :rails_env,   "staging"
 set :deploy_to,   "/data/#{fetch(:application)}-dev"
 set :deploy_user, "app-#{fetch(:application)}"
 
+set :branch, "dev"
+
 server "chi-dev", user: 'app-afscme-tk', roles: %w(web app db)
