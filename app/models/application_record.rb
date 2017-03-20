d9720ba1ebc6ba636195664ef9209fa6789f3362
Jarred Trost <jtrost@gmail.com>
2017-03-20 14:12:20 -0500

    Try forcing https

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -5,7 +5,7 @@ class ApplicationRecord < ActiveRecord::Base
 
   def s3_credentials
     {
-      s3_protocol:      "http",
+      s3_protocol:      "https",
       s3_region:        "us-east-1",
       bucket:           "toolkit.afscme.org",
       access_key_id:     Rails.application.secrets.aws["access_key_id"],
