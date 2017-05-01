0c9913565c87d7a87ad60ee1d10eef43eea7f8ee
Jarred Trost <jtrost@gmail.com>
2017-05-01 14:28:30 -0500

    Adding this method back in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,13 +3,19 @@ module ApplicationHelper
     "#{template.width}\" x #{template.height}\""
   end
 
-  ## touching this file
-
   def download_link(document)
     if document.pdf_file_name
       document.pdf.url
     else
       download_document_path(document)
+    end
+  end
+
+  def thumbnail_url(document)
+    if document.thumbnail_file_name
+      document.thumbnail.url
+    else
+      document.template.thumbnail.url
     end
   end
 end
