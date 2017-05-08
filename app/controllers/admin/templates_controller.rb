4fca22c80c4b4d341c850d54c3c11943f4ce684e
Jarred Trost <jtrost@gmail.com>
2017-05-08 09:59:07 -0500

    Updating image upload fields

--- app/controllers/admin/templates_controller.rb
+++ app/controllers/admin/templates_controller.rb
@@ -75,7 +75,7 @@ class Admin::TemplatesController < AdminController
     params.require(:template).permit(
       :title, :description, :height, :width, :pdf_markup, :form_markup, :status, :thumbnail,
       :numbered_image, :blank_image, :customizable_options, :campaign_id, :category_id,
-      :customize, :static_pdf
+      :customize, :static_pdf, :orientation
     )
   end
 end
