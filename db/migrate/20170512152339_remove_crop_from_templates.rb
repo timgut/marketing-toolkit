65ca681185bfa380fdbf49a22836b19d1653e4ba
Jarred Trost <jtrost@gmail.com>
2017-05-17 10:22:07 -0500

    Uncomment

--- db/migrate/20170512152339_remove_crop_from_templates.rb
+++ db/migrate/20170512152339_remove_crop_from_templates.rb
@@ -1,7 +1,7 @@
 class RemoveCropFromTemplates < ActiveRecord::Migration[5.0]
   def change
-    # remove_column :templates, :crop_top
-    # remove_column :templates, :crop_bottom
+    remove_column :templates, :crop_top
+    remove_column :templates, :crop_bottom
     remove_column :templates, :blank_image_height
     remove_column :templates, :blank_image_width
   end
