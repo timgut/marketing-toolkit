7f479cb27664e52e1a013606b560db45f627c043
Jarred Trost <jtrost@gmail.com>
2017-06-01 15:26:00 -0500

    Almost finished with documents controller specs

--- spec/controllers/documents_controller_spec.rb
+++ spec/controllers/documents_controller_spec.rb
@@ -32,6 +32,19 @@ RSpec.describe DocumentsController, type: :controller do
     { id: document.id }
   end
 
+  def new_params
+    { template_id: template.id }
+  end
+
+  def update_params
+    {
+      id:          document.id,
+      document:    {title: "New Title"},
+      data:        [{key: "new_key", value: "new value"}],
+      select_data: {new_key: "fieldId"}
+    }
+  end
+
   def mock_failure(method)
     allow_any_instance_of(Document).to receive(method).and_return(false)
   end
@@ -117,7 +130,7 @@ RSpec.describe DocumentsController, type: :controller do
     end
   end
 
-  describe "GET #show" do
+  describe "GET #edit" do
     context "not signed in" do
       it "redirects to the sign in page" do
         get :edit, params: destroy_params
@@ -141,4 +154,200 @@ RSpec.describe DocumentsController, type: :controller do
     end
   end
 
+  describe "GET #download" do
+    context "not signed in" do
+      it "redirects to the sign in page" do
+        get :download, params: destroy_params
+        expect(response).to have_http_status 302
+        expect(response.body).to eq RSpec.configuration.redirect_html
+      end
+    end
+
+    RSpec.configuration.user_roles.each do |role|
+      context "signed in as: #{role}" do
+        __send__("controller_#{role}_sign_in".to_sym)
+
+        it "downloads the PDF" do
+          own_document
+          get :edit, params: destroy_params
+
+          expect(response).to have_http_status 200
+        end
+      end
+    end
+  end
+
+  [:recent, :shared, :index].each do |action|
+    describe "GET ##{action}" do
+      context "not signed in" do
+        it "redirects to the sign in page" do
+          get action
+          expect(response).to have_http_status 302
+          expect(response.body).to eq RSpec.configuration.redirect_html
+        end
+      end
+
+      RSpec.configuration.user_roles.each do |role|
+        context "signed in as: #{role}" do
+          __send__("controller_#{role}_sign_in".to_sym)
+
+          it "renders the index template" do
+            get action
+
+            expect(response).to have_http_status 200
+            expect(response).to render_template "index"
+          end
+        end
+      end
+    end
+  end
+
+  describe "GET #new" do
+    context "not signed in" do
+      it "redirects to the sign in page" do
+        get :new, params: new_params
+
+        expect(response).to have_http_status 302
+        expect(response.body).to eq RSpec.configuration.redirect_html
+      end
+    end
+
+    RSpec.configuration.user_roles.each do |role|
+      context "signed in as: #{role}" do
+        __send__("controller_#{role}_sign_in".to_sym)
+
+        it "renders the new template" do
+          get :new, params: new_params
+
+          expect(response).to have_http_status 200
+          expect(response).to render_template "new"
+        end
+      end
+    end
+  end
+
+  describe "GET #preview" do
+    context "not signed in" do
+      it "redirects to the sign in page" do
+        get :preview, params: destroy_params
+        expect(response).to have_http_status 302
+        expect(response.body).to eq RSpec.configuration.redirect_html
+      end
+    end
+
+    RSpec.configuration.user_roles.each do |role|
+      context "signed in as: #{role}" do
+        __send__("controller_#{role}_sign_in".to_sym)
+
+        it "renders the build template" do
+          own_document
+          get :preview, params: destroy_params
+
+          expect(response).to have_http_status 200
+          expect(response).to render_template "documents/build.pdf.erb"
+        end
+      end
+    end
+  end
+
+  describe "PATCH #update" do
+    context "not signed in" do
+      it "redirects to the sign in page" do
+        patch :update, params: update_params
+
+        expect(response).to have_http_status 302
+        expect(response.body).to eq RSpec.configuration.redirect_html
+      end
+    end
+
+    RSpec.configuration.user_roles.each do |role|
+      context "signed in as: #{role}" do
+        __send__("controller_#{role}_sign_in".to_sym)
+
+        context "when successful" do
+          it "redirects to the document edit page" do
+            own_document
+            patch :update, params: update_params
+            
+            expect(response).to have_http_status 302
+            expect(response.body).to redirect_to edit_document_path(document)
+          end
+        end
+
+        context "when failure" do
+          it "renders the edit template" do
+            own_document
+            mock_failure(:update_attributes)
+            patch :update, params: update_params
+
+            expect(response).to have_http_status 302
+            expect(response.body).to redirect_to RSpec.configuration.http_referer
+          end
+        end
+      end
+    end
+  end
+
+  describe "Trashable concern" do
+    describe "DELETE #destroy" do
+      context "not signed in" do
+        it "redirects to the sign in page" do
+          delete :destroy, params: destroy_params
+
+          expect(response).to have_http_status 302
+          expect(response).to redirect_to RSpec.configuration.http_referer
+        end
+      end
+
+      RSpec.configuration.user_roles.each do |role|
+        __send__("controller_#{role}_sign_in".to_sym)
+
+        context "signed in as: #{role}" do
+          context "when the document is in the trash" do
+            context "when successful" do
+              it "redirects to the documents index" do
+                own_document
+                document.update_attributes(status: -2)
+                delete :destroy, params: destroy_params
+
+                expect(response).to have_http_status 302
+                expect(response).to redirect_to "#{documents_path}?notice=Document+destroyed%21"
+              end
+            end
+
+            context "when failure" do
+              it "redirects to the documents index" do
+                own_document
+                document.update_attributes(status: -2)
+                mock_failure(:destroy)
+                delete :destroy, params: destroy_params
+
+                expect(response).to have_http_status 302
+                expect(response).to redirect_to "#{documents_path}?alert=Cannot+destroy+document.+Please+try+again."
+              end
+            end
+          end
+        end
+
+        context "when the document is NOT in the trash" do
+          it "redirects to the documents index" do
+            own_document
+            delete :destroy, params: destroy_params
+
+            expect(response).to have_http_status 302
+            expect(response).to redirect_to "#{documents_path}?alert=This+document+must+be+placed+in+the+trash+before+it+can+be+destroyed."
+          end
+        end
+      end
+    end
+
+    describe "PATCH #restore" do
+
+    end
+
+    describe "PATCH #trash" do
+
+    end
+
+  end
 end
