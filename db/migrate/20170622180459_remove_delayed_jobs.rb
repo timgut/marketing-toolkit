ea0efe23f494dbf9699e7d1934fac06bb7eb4a56
Tim Gutowski <tgutowski@trilogyinteractive.com>
2017-06-30 15:46:40 -0500

    including the thumbnail migration in master now

--- db/migrate/20170622180459_remove_delayed_jobs.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDelayedJobs < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :delayed_jobs
-  end
-end
---
+++ db/migrate/20170630204529_add_thumbnail_to_documents.rb
@@ -0,0 +1,5 @@
+class AddThumbnailToDocuments < ActiveRecord::Migration[5.0]
+  def change
+    add_attachment :documents, :thumbnail
+  end
+end
