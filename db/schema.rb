f6d9831b283222d2b628a9e632118839bb0181e5
Tim Gutowski <tgutowski@trilogyinteractive.com>
2017-12-22 11:11:25 -0600

    adding Google Analytics

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170721183442) do
+ActiveRecord::Schema.define(version: 20170801185704) do
 
   create_table "affiliates", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "title"
