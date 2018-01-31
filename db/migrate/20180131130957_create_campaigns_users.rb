718b233078f64f08feba6b36f00c30416ab9e22c
Jarred Trost <jtrost@gmail.com>
2018-01-31 10:34:42 -0600

    clean up migrations

--- db/migrate/20180131130957_create_campaigns_users.rb
+++ db/migrate/20180131130957_create_campaigns_users.rb
@@ -1,6 +1,6 @@
-class CreateCampaignUsers < ActiveRecord::Migration[5.0]
+class CreateCampaignsUsers < ActiveRecord::Migration[5.0]
   def change
-    create_table :campaign_users do |t|
+    create_table :campaigns_users do |t|
       t.belongs_to :campaign
       t.belongs_to :user
       t.timestamps
