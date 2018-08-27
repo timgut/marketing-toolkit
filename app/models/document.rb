137e4202e780c77b738784d95a2ab9090ed5123b
Jarred Trost <jtrost@gmail.com>
2018-08-27 13:52:56 -0500

    Fixed query

--- app/models/document.rb
+++ app/models/document.rb
@@ -22,7 +22,7 @@ class Document < ApplicationRecord
   scope :recent,         ->(user) { where("documents.creator_id = ? AND documents.created_at >= ?", user.id, DateTime.now - 2.weeks) }
   scope :shared_with_me, ->(user) { all.joins(:documents_users).where("user_id = ? and documents_users.user_id != ?", user.id, user.id) }
   
-  scope :rendered, -> { where("pdf_file_name != ? OR share_graphic_file_name != ?", nil, nil) }
+  scope :rendered, -> { where.not(pdf_file_name: nil).or(where.not(share_graphic_file_name: nil)) }
   scope :newest,   -> { order(created_at: :desc) }
 
   before_destroy ->{ self.pdf = nil; self.share_graphic = nil; self.thumbnail = nil }
