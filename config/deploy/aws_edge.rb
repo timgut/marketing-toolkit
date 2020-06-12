c8196ea8a0db22b22810458fcfb17bc2f7c59042
Tim Gutowski <tgutowski@trilogyinteractive.com>
2020-06-12 15:03:12 -0500

    added full_application to aws_edge deploy config

--- config/deploy/aws_edge.rb
+++ config/deploy/aws_edge.rb
@@ -5,6 +5,9 @@ set :deploy_user, "app-afscme-tk-edge"
 set :branch,      "edge"
 set :default_env, { 'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH" }
 
+set :full_application, "afscme-tk-edge"
+
+
 # SSHKit.config.command_map[:bundle] = "bundle"
 
 server "chi-dev", user: "app-afscme-tk-edge", roles: %w(web app db)
