bbd9a03ab840f4e78d1452c33831a8a61b9e8ccb
Jarred Trost <jtrost@gmail.com>
2020-06-16 13:24:41 -0500

    test change

--- config/deploy/aws_edge.rb
+++ config/deploy/aws_edge.rb
@@ -3,6 +3,9 @@ set :rails_env,   "staging"
 set :deploy_to,   "/data/afscme-tk-edge"
 set :deploy_user, "app-afscme-tk-edge"
 set :branch,      "edge"
+set :default_env, { 'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH" }
 set :full_application, "afscme-tk-edge"
+
+SSHKit.config.command_map[:bundle] = "bundle"
 
 server "chi-dev", user: "app-afscme-tk-edge", roles: %w(web app db)
