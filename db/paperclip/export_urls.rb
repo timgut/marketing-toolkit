c9ca9b934a5927bd7ebe2310a373f4a46f66ccac
Jarred Trost <jtrost@gmail.com>
2020-10-21 13:16:22 -0500

    fixing typos

--- db/paperclip/export_urls.rb
+++ db/paperclip/export_urls.rb
@@ -1,6 +1,6 @@
 # Run with: bin/rails c
 # load Rails.root.join("db", "paperclip", "export_urls.rb")
-CSV.open(Rails.root.join("db", "paperclip", "data", "image_urls.csv", "wb")) do |csv|
+CSV.open(Rails.root.join("db", "paperclip", "data", "image_urls.csv"), "wb") do |csv|
   Image.all.each do |image|
     csv << [
       image.image.url(:original).sub("/system", ""),
@@ -9,23 +9,24 @@ CSV.open(Rails.root.join("db", "paperclip", "data", "image_urls.csv", "wb")) do 
   end
 end
 
-CSV.open(Rails.root.join("db", "paperclip", "data", "template_urls.csv", "wb")) do |csv|
+CSV.open(Rails.root.join("db", "paperclip", "data", "template_urls.csv"), "wb") do |csv|
   Template.all.each do |template|
     csv << [
-    template.thumbnail.url.sub("/system", ""),
-    template.numbered_image.url.sub("/system", ""),
-    template.blank_image.url.sub("/system", ""),
-    template.static_pdf.url.sub("/system", "")
-  ]
+      template.thumbnail.url.sub("/system", ""),
+      template.numbered_image.url.sub("/system", ""),
+      template.blank_image.url.sub("/system", ""),
+      template.static_pdf.url.sub("/system", "")
+    ]
+  end
 end
 
-CSV.open(Rails.root.join("db", "paperclip", "data", "stock_image_urls.csv", "wb")) do |csv|
+CSV.open(Rails.root.join("db", "paperclip", "data", "stock_image_urls.csv"), "wb") do |csv|
   StockImage.all.each do |image|
     csv << [image.update_attributes!(image_url: image.image.url.sub("/system", ""))]
   end
 end
 
-CSV.open(Rails.root.join("db", "paperclip", "data", "document_urls.csv", "wb")) do |csv|
+CSV.open(Rails.root.join("db", "paperclip", "data", "document_urls.csv"), "wb") do |csv|
   Document.all.each do |document|
     csv << [
       document.pdf.url.sub("/system", ""),
