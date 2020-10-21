996df106255ec19847608eeb8ce522e79847ea36
Jarred Trost <jtrost@gmail.com>
2020-10-21 13:51:22 -0500

    Added paperclip url files

---
+++ db/paperclip/export_urls.rb
@@ -0,0 +1,37 @@
+# Run with: bin/rails c
+# load Rails.root.join("db", "paperclip", "export_urls.rb")
+CSV.open(Rails.root.join("db", "paperclip", "data", "image_urls.csv"), "wb") do |csv|
+  Image.all.each do |image|
+    csv << [
+      image.image.url(:original).sub("/system", ""),
+      image.image.url(:cropped).sub("/system", "")
+    ]
+  end
+end
+
+CSV.open(Rails.root.join("db", "paperclip", "data", "template_urls.csv"), "wb") do |csv|
+  Template.all.each do |template|
+    csv << [
+      template.thumbnail.url.sub("/system", ""),
+      template.numbered_image.url.sub("/system", ""),
+      template.blank_image.url.sub("/system", ""),
+      template.static_pdf.url.sub("/system", "")
+    ]
+  end
+end
+
+CSV.open(Rails.root.join("db", "paperclip", "data", "stock_image_urls.csv"), "wb") do |csv|
+  StockImage.all.each do |image|
+    csv << [image.image.url.sub("/system", "")]
+  end
+end
+
+CSV.open(Rails.root.join("db", "paperclip", "data", "document_urls.csv"), "wb") do |csv|
+  Document.all.each do |document|
+    csv << [
+      document.pdf.url.sub("/system", ""),
+      document.thumbnail.url.sub("/system", ""),
+      document.share_graphic.url.sub("/system", "")
+    ]
+  end
+end
